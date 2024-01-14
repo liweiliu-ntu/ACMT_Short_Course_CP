8d7b7af9338575556095e57915123688e632345b
Cheng-Yuan-Chen <118056075+Cheng-Yuan-Chen@users.noreply.github.com>
2024-01-15 01:22:28 +0800

    Add files via upload

---
+++ ACMT code bilinearGPS/ACMTmain_bilinear.m
@@ -0,0 +1,260 @@
+clc;
+clear all;
+close all;
+%% parameters
+dt=0.005;
+ke=1000;
+kp=500;
+Qy=1000;
+ee=1e-10;
+%Qy=1;
+%% input data
+t=0:dt:20;
+Q11=(t.^2)';
+Q12=(2*t.^2)';
+Q21=(0*t.^2)';
+Q22=(0*t.^2)';
+Q31=(0*t.^2)';
+Q32=(0*t.^2)';
+Q=[ Q11  Q12  Q21  Q22  Q31  Q32 ];
+% Q(1,:)=[-50 ; 50 ; 0 ; 0 ; 0 ; 0 ];
+%% A matrix
+beta=(ke)/(ke+kp);
+qy=Qy/ke;
+%% initial values
+q=[Qy/ke ; 0 ; 0 ; 0 ; 0 ; 0 ]';
+dq=[0 ; 0 ; 0 ; 0 ; 0 ; 0 ];
+dQ=[1 ; 1 ; 0 ; 0 ; 0 ; 0 ];
+qp=[0 ; 0 ; 0 ; 0 ; 0 ; 0 ];
+dqp=[0 ; 0 ; 0 ; 0 ; 0 ; 0 ];
+Qa=[0 ; 0 ; 0 ; 0 ; 0 ; 0 ];
+Qb=[0 ; 0 ; 0 ; 0 ; 0 ; 0 ];
+% straining condition
+s=0;
+% switching index
+iflag=[0];
+state1=[0 dq'];
+state2=[0 q dQ' Qb' Qa' norm(Qa)-Qy qp' Q11(1) Q12(1) Q21(1) Q22(1) Q31(1) Q32(1) iflag];
+
+%%
+tic;
+for ii=2:length(Q11)
+    %ggg=ii;
+       if iflag==[0];
+        % off_phase
+        [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp]=off_phase(q,ke,kp,dt,Qy,Q,Qb,ee,ii);
+       if nextyf>0
+            % off to on
+            [ts,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextQ] = pull_back (iflag,dQ,q,ke,kp,dt,Qy,Q,Qb,ee,ii);
+           
+           Q1=Q(ii-1,:)+dQ*ts; q=nextq; dq=nextdq; dQ=nextdQ; Qb=nextQb; Qa=Q1; qp=nextqp;
+            state1=[state1;(ii-2)*dt+ts dq ];
+            state2=[state2; [(ii-2)*dt+ts q dQ Qb' Qa nextyf qp' Q1 iflag]];
+          
+            iflag=[1];
+            [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextdqp]=on_phase(q,ke,kp,dt-ts,Qy,Q,Qb,qp,Qa,qy,beta,ee,ii);
+       else
+           iflag=[0];
+       end
+       elseif iflag==[1];
+             s=(Qa*dQ')/(kp*Qy);
+             if s>0 
+             iflag=[1];
+            [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextdqp]=on_phase(q,ke,kp,dt,Qy,Q,Qb,qp,Qa,qy,beta,ee,ii); 
+               else 
+                   iflag=[0];
+
+                     [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp]=off_phase(q,ke,kp,dt,Qy,Q,Qb,ee,ii);
+            
+                   if nextyf>0
+            % off to on
+                    [ts,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextQ] = pull_back (iflag,dQ,q,ke,kp,dt,Qy,Q,Qb,ee,ii);
+                     Q1=Q(ii-1,:)+dQ*ts;q=nextq; dq=nextdq; dQ=nextdQ; Qb=nextQb; Qa=Q1; qp=nextqp;
+                    state1=[state1;(ii-2)*dt+ts dq ];
+                    state2=[state2; [(ii-2)*dt+ts q dQ Qb' Qa nextyf qp' Q1 iflag]];
+                   
+                    iflag=[1];
+                    [nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextdqp]=on_phase(q,ke,kp,dt-ts,Qy,Q,Qb,qp,Qa,qy,beta,ee,ii);
+      
+           
+                   else
+                    iflag=[0];
+                   end
+
+                   
+               end
+       end
+    Q1=nextQ;
+    q=nextq;
+    dq=nextdq;
+    dQ=nextdQ;
+    Qb=nextQb;
+    Qa=nextQa;
+    qp=nextqp;
+    state1=[state1;(ii-1)*dt dq];
+    state2=[state2;[(ii-1)*dt q dQ Qb' Qa nextyf qp' Q1 iflag]];
+
+end
+toc;
+% figure(1)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,33),state2(:,34),'linewidth',1.0)
+% title('q-Q');
+% xlabel('q');
+% ylabel('Q');
+% 
+for i=1:length(state2)
+    f1(i)=norm(state2(i,20:25))-Qy;
+end
+f1=f1';
+% figure(2)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,3),state2(:,34),'linewidth',1.0)
+% title('q-Q');
+% xlabel('q');
+% ylabel('Q');
+% 
+% figure(3)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,33),'linewidth',1.0)
+% title('q-Q');
+% xlabel('q');
+% ylabel('Q');
+% 
+% figure(4)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,34),'linewidth',1.0)
+% title('q-Q');
+% xlabel('q');
+% ylabel('Q');
+
+% figure(2)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,5),'linewidth',1.0)
+% title('X-direction Displacement Response');
+% xlabel('Time (s)');
+% ylabel('Displacement  q_x (cm)');
+% 
+% figure(3)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,8),'linewidth',1.0)
+% title('X-direction Restoring-Force Response');
+% xlabel('Time (s)');
+% ylabel(' Restoring-force  Q_x (kgf)');
+% 
+% figure(4)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,11),'linewidth',1.0)
+% title('X-direction Back-Force Response');
+% xlabel('Time (s)');
+% ylabel('Back_Force  Qb_x (kgf)');
+% 
+% figure(5)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,14),'linewidth',1.0)
+% title('X-direction Active-Force Response');
+% xlabel('Time (s)');
+% ylabel('Active_Force  Qa_x (kgf)');
+% 
+% 
+% 
+% 
+% figure(6)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,5),state2(:,8)-c*state2(:,2)/m,'linewidth',1.0)
+% title('X-direction Q_x-q_x Response');
+% xlabel('Displacement  q_x (cm)');
+% ylabel('Total force  Q_x (kgf)');
+% 
+% figure(7)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,5),state2(:,8),'linewidth',1.0)
+% title('X-direction Q_x-q_x Response');
+% xlabel('Displacement  q_x (cm)');
+% ylabel('Total force  Q_x (kgf)');
+% 
+%  figure(8)
+%  axes('FontSize',14,'FontName','TimesNewRoman');
+%  plot(state2(:,1),state2(:,21),'linewidth',1.0)
+%  title('Iflag');
+%  xlabel('Time(s)');
+%  ylabel('Iflag');
+% t1=[0:dt:dt*(length(ag)-1)]';
+% 
+%  figure(9)
+%  axes('FontSize',14,'FontName','TimesNewRoman');
+%  plot(t1,ag(:,1),'linewidth',1.0)
+%  title('Input force');
+%  xlabel('Time(s)');
+%  ylabel('Force');
+% 
+% figure(10)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state2(:,1),state2(:,2)/m,'linewidth',1.0)
+% title('X-direction velocity Response');
+% xlabel('Time (s)');
+% ylabel('velocity  v_x (kg*cm/s)');
+% 
+% figure (11)
+% axes('FontSize',14,'FontName','TimesNewRoman');
+% plot(state1(:,1),state1(:,2)/m,'linewidth',1.0)
+% title('X-direction acceleration Response');
+% xlabel('Time (s)');
+% ylabel('acceleration  a_x (kg*cm/s^2)');
+% 
+% 
+% % figure (12)
+% % axes('FontSize',14,'FontName','TimesNewRoman');
+% % plot(state1(:,1),state1(:,5),'linewidth',1.0)
+% % title('X-direction acceleration Response');
+% % xlabel('Time (s)');
+% % ylabel('acceleration  a_x (kg*cm/s^2)');
+% peakvalue1=max(abs(state2(:,5)))
+% peakvalue2=max(abs(state2(:,6)))
+% peakvalue3=max(abs(state2(:,8)))
+% peakvalue4=max(abs(state2(:,9)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ACMT code bilinearGPS/pull_back.m
@@ -0,0 +1,42 @@
+function [ts,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp,nextQ] = pull_back (iflag,dQ,q,ke,kp,dt,Qy,Q,Qb,ee,ii)
+ syms ts
+ f=Q(ii-1,:)+dQ*ts;
+ f1=norm(f)-Qy;
+ ts1=solve(f1,ts);
+ts2=double(ts1);
+ for jj=1:length(ts2)
+     if ts2(jj)>0
+         ts=ts2(jj);
+     end
+ end
+ ts;
+ [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp]=off_phase(q,ke,kp,ts,Qy,Q,Qb,ee,ii);
+
+% tolrance=1e-9;
+% t_start=0.0;
+% tau=0.01;
+% tempp=-Qy;
+% iit=0;
+% %
+% while abs(tempp) > tolrance
+%     iit=iit+1;
+%     tau=tau/10;
+%     for jj=2:10
+%         if iflag==[0]
+%             [nextQ,nextq,nextdq,nextdQ,nextQb,nextQa,nextyf,nextqp]=off_phase(q,ke,kp,t_start+tau*(jj-1),Qy,Q,Qb,ee,ii);
+%         end
+%         tempp=nextyf;
+%         if tempp > tolrance
+%             t_start=t_start+tau*(jj-1);
+%             break      
+%         end
+%     end
+%     if (iit > 10000)
+%         fprintf('pull-back warning,');
+%         fprintf('\t tempp=%8.5e,',tempp);
+%         fprintf('\t t_start=%8.5e,',t_start); 
+%         break
+%     end
+% end
+% ts=t_start+tau*jj;
+         
